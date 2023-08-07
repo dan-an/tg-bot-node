77f715540a396ec3c7e9d714503b4b0a8d907c48
Danilov Andrey <a.danilov@kryptonite.ru>
2023-08-07 12:44:23 +0300

    ni: moved routes to external files

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,13 @@
 import fastify from "fastify";
 import { GoogleSpreadsheet } from 'google-spreadsheet'
-import axios from 'axios'
 import { config } from 'dotenv'
 import { JWT } from 'google-auth-library'
 import * as process from "process";
 // @ts-ignore
 import {findFilm} from "./services/kinopoisk.ts";
+import NewMessage from "@/routes/newMessage";
 
 config()
-
-const TELEGRAM_URI = `https://api.telegram.org/bot${process.env.TELEGRAM_API_TOKEN}/sendMessage`
 
 const server = fastify()
 
@@ -25,45 +23,10 @@ const serviceAccountAuth = new JWT({
 
 const doc = new GoogleSpreadsheet(process.env.GOOGLE_FILM_LIST_ID!, serviceAccountAuth)
 
-const messageText = 'человек паук'
+// Register routes
+server.register(NewMessage)
 
-server.post('/new-message', async (req, res) => {
-    // @ts-ignore
-    const { message } = req.body
-
-    const messageText = message?.text?.toLowerCase()?.trim()
-    const chatId = message?.chat?.id
-    if (!messageText || !chatId) {
-        return res.code(400)
-    }
-
-    let films = await findFilm(messageText)
-
-    const formattedFilmList = films.map((film: any, index: number) => {
-        const {name, rating, year, shortDescription} = film
-        return `<b>${index + 1}. ${name} (${year})</b>\n<i>kp: ${rating.kp}, imdb: ${rating.imdb}</i>\n${shortDescription}`
-    }).join('\n\n')
-
-    const replyText = `<b>Нашлось ${films.length} фильмов:</b>\n\n${formattedFilmList}`
-
-    const inlineKeyboardMarkup = {
-        inline_keyboard: films.map((film: any, index: number) => {
-            return [{text: index + 1, callback_data: film.id}]
-        })
-    }
-
-    try {
-        await axios.post(TELEGRAM_URI, {
-            parse_mode: "HTML",
-            chat_id: chatId,
-            text: replyText,
-            reply_markup: JSON.stringify(inlineKeyboardMarkup)
-        })
-    } catch (e) {
-        console.log(e)
-    }
-})
-
+// start
 const start = async () => {
     try {
         await server.listen({ port: parseInt(process.env.PORT!), host: '0.0.0.0' });
---
+++ src/services/tg.ts
@@ -0,0 +1,41 @@
+import {findFilm} from "@/services/kinopoisk";
+import axios from "axios";
+import {config} from "dotenv";
+
+config()
+
+const TELEGRAM_URI = `https://api.telegram.org/bot${process.env.TELEGRAM_API_TOKEN}/sendMessage`
+const handleNewMessage = async (message: any) => {
+    const messageText = message?.text?.toLowerCase()?.trim()
+    const chatId = message?.chat?.id
+    if (!messageText || !chatId) {
+        throw new HttpError('No message text or chat id', 400)
+    }
+
+    let films = await findFilm(messageText)
+
+    const formattedFilmList = films.map((film: any, index: number) => {
+        const {name, rating, year, shortDescription} = film
+        return `<b>${index + 1}. ${name} (${year})</b>\n<i>kp: ${rating.kp}, imdb: ${rating.imdb}</i>\n${shortDescription}`
+    }).join('\n\n')
+
+    const replyText = `<b>Нашлось ${films.length} фильмов:</b>\n\n${formattedFilmList}`
+
+    const inlineKeyboardMarkup = {
+        inline_keyboard: films.map((film: any, index: number) => {
+            return [{text: index + 1, callback_data: film.id}]
+        })
+    }
+
+    try {
+        await axios.post(TELEGRAM_URI, {
+            parse_mode: "HTML",
+            chat_id: chatId,
+            text: replyText,
+            reply_markup: JSON.stringify(inlineKeyboardMarkup)
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+export default handleNewMessage
