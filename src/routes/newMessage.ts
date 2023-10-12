4e16823cc82424190a7197aac094eb5f0ebe9166
Danilov Andrey <a.danilov@kryptonite.ru>
2023-10-12 23:40:39 +0300

    ni: refactored logic

--- src/routes/newMessage.ts
+++ src/routes/newMessage.ts
@@ -4,7 +4,8 @@ import fp from 'fastify-plugin';
 import handleNewMessage from "../services/tg/index.ts";
 // @ts-ignore
 import {HttpError} from "../types/index.ts";
-import logIncome from "@/services/tg/logger";
+// @ts-ignore
+import logIncome from "../services/tg/logger.ts";
 
 const NewMessage: FastifyPluginAsync = async (server: FastifyInstance)=> {
     server.post('/new-message', async (request, reply) => {
