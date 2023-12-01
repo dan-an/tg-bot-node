026fc2aeb874dcfcaa1ad089c58e0be55b699f14
Danilov Andrey <a.danilov@kryptonite.ru>
2023-12-02 00:39:01 +0300

    added shoplist handling

--- src/services/tg/dictionary.ts
+++ src/services/tg/dictionary.ts
@@ -2,11 +2,17 @@
 import {PhraseConfig} from "../../types/index.ts"
 
 export const userRequests: PhraseConfig = {
-    save: ['запомни фильм', 'save']
+    save: ['запомни фильм', 'save'],
+    shoplist: ['сохрани в покупки', 'shopping']
+}
+
+export enum hashtags {
+    FILMS = 'кино',
+    SHOPPING = 'покупки',
 }
 
 export const botReplies: PhraseConfig = {
     nothingFound: ['Я такого не нашла('],
     askHelp: ['Помоги выбрать', 'Не могу определиться', 'Есть из чего выбрать'],
-    forceUser: ['Давай название!)', 'Диктуй!'],
+    forceUser: ["Записываю)", 'Диктуй!'],
 }
