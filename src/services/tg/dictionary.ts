055e1140f218b6d239f58503920986083deb86f7
Danilov Andrey <a.danilov@kryptonite.ru>
2023-12-04 16:25:29 +0300

    added threads support for shopping list feature

--- src/services/tg/dictionary.ts
+++ src/services/tg/dictionary.ts
@@ -11,8 +11,17 @@ export enum hashtags {
     SHOPPING = 'покупки',
 }
 
+export enum categories {
+    FOOD = "продукты",
+    CLOTHES = "одежда",
+    CHEMISTRY = "бытовая химия",
+    CAR = "в машину",
+    OTHER = "другое"
+}
+
 export const botReplies: PhraseConfig = {
     nothingFound: ['Я такого не нашла('],
     askHelp: ['Помоги выбрать', 'Не могу определиться', 'Есть из чего выбрать'],
     forceUser: ["Записываю)", 'Диктуй!'],
+    askCategory: ['А какая это категория товаров?']
 }
