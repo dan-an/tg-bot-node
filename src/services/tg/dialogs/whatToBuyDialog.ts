c260cacdeabd5089de6fe6972e2ccb51b7e6a10f
Danilov Andrey <a.danilov@kryptonite.ru>
2023-12-11 17:20:26 +0300

    ni: moved shopping list getting logic to dialog file

---
+++ src/services/tg/dialogs/whatToBuyDialog.ts
@@ -0,0 +1,123 @@
+// @ts-ignore
+import {botReplies, categories, columns, filters, hashtags, userRequests} from "../dictionary.ts";
+// @ts-ignore
+import {googleInstance} from "../../../app.ts";
+// @ts-ignore
+import {TelegramBot} from "../../types/index.ts";
+// @ts-ignore
+import {getRandomPhrase, sendMessage} from "../tools.ts";
+import {EventEmitter} from "events"
+import {config} from "dotenv";
+
+
+config()
+
+export class WhatToBuyDialog extends EventEmitter {
+    chatId = ''
+    isReplyToBot = false
+    filterColumn = ''
+    filterValue = ''
+
+    public async handleNewMessage(message: TelegramBot.Message) {
+        this.chatId = message?.chat?.id
+        this.isReplyToBot = message.reply_to_message && message.reply_to_message.from.username && message.reply_to_message.from.username.toLowerCase()?.trim() === process.env.TELEGRAM_BOT_NAME
+
+        const reply: TelegramBot.Message = {
+            chat_id: this.chatId,
+            parse_mode: "HTML",
+        }
+
+        if (!this.isReplyToBot) {
+            const keyboard: TelegramBot.InlineKeyboardMarkup = {
+                inline_keyboard: (Object.entries(filters) as string[][]).reduce<{
+                    text: string, callback_data: string
+                }[][]>((keyboard, column: string[]) => {
+                    if (!keyboard.length || keyboard.at(-1)!.length === 2) {
+                        keyboard.push([])
+                    }
+
+                    if (keyboard.at(-1)!.length < 2) {
+                        keyboard.at(-1)!.push({
+                            text: column[1],
+                            callback_data: JSON.stringify({
+                                type: "filterColumn",
+                                data: column[1] !== filters.NONE ? column[0] : ''
+                            })
+                        })
+                    }
+
+                    return keyboard
+                }, []),
+            }
+
+            reply.text = botReplies.askFilter[0]
+            reply.reply_markup = JSON.stringify(keyboard)
+            await sendMessage(reply)
+        }
+    }
+
+    public async handleCallbackQuery(payload: any) {
+        const {message} = payload
+
+        this.chatId = message?.chat?.id
+
+        if (payload.data) {
+            const reply: TelegramBot.Message = {
+                chat_id: this.chatId,
+                text: '',
+                parse_mode: "HTML",
+            }
+
+            const parsedPayload = JSON.parse(payload.data)
+
+
+            if (parsedPayload.type) {
+                switch (parsedPayload.type) {
+                    case "filterColumn":
+                        this.filterColumn = parsedPayload.data ? filters[parsedPayload.data] : ""
+
+                        if (!this.filterColumn) {
+                            break
+                        }
+
+                        reply.text = 'Отлично, что выберем?'
+                        reply.reply_markup = {
+                            inline_keyboard: (Object.values(categories) as string[]).reduce<{
+                                text: string, callback_data: string
+                            }[][]>((keyboard, category: string) => {
+                                if (!keyboard.length || keyboard.at(-1)!.length === 2) {
+                                    keyboard.push([])
+                                }
+
+                                if (keyboard.at(-1)!.length < 2) {
+                                    keyboard.at(-1)!.push({
+                                        text: category, callback_data: JSON.stringify({
+                                            type: 'filterValue',
+                                            data: category
+                                        })
+                                    })
+                                }
+
+                                return keyboard
+                            }, []),
+                        }
+                        await sendMessage(reply)
+                        return
+                    case "filterValue":
+                        this.filterValue = parsedPayload.data
+                }
+            }
+            const shopItems = await googleInstance.getRows(process.env.SHOPPING_SHEET_ID, this.filterColumn, this.filterValue)
+
+            const formattedList = shopItems.map((item: any) => {
+                return `    - ${item[columns.NAME]}`
+            })
+
+            reply.text = `<b>Надо купить: </b>\n${formattedList.join('\n')}`
+
+            await sendMessage(reply)
+
+            this.emit('dialog is over')
+        }
+    }
+}
