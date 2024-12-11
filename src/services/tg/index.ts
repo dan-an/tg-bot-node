7b39b1f3726279bb0e30146b9750f17400c72d9c
Danilov Andrey <a.danilov@kryptonite.ru>
2024-12-11 11:26:02 +0300

    ni: set up prettier & reformat code

--- src/services/tg/index.ts
+++ src/services/tg/index.ts
@@ -1,11 +1,11 @@
-import {config} from "dotenv";
-import {TelegramBot} from "@/types/telegram";
-import {SaveFilmDialog} from "@/services/tg/dialogs/saveFilmDialog"
-import {ShoppingDialog} from "@/services/tg/dialogs/shoppingDialog";
-import {WhatToBuyDialog} from "@/services/tg/dialogs/whatToBuyDialog";
-import {sendMessage} from "@/services/tg/tools";
+import { config } from 'dotenv';
+import { TelegramBot } from '@/types/telegram';
+import { SaveFilmDialog } from '@/services/tg/dialogs/saveFilmDialog';
+import { ShoppingDialog } from '@/services/tg/dialogs/shoppingDialog';
+import { WhatToBuyDialog } from '@/services/tg/dialogs/whatToBuyDialog';
+import { sendMessage } from '@/services/tg/tools';
 
-config()
+config();
 
 type DialogInstance = {
     handleNewMessage: (message: TelegramBot.Message) => Promise<void>;
@@ -20,17 +20,17 @@ const dialogs: Record<string, new () => DialogInstance> = {
 };
 
 export class TelegramController {
-    hasNeededMeta = false
+    hasNeededMeta = false;
     messageMeta: TelegramBot.MessageEntity | null = null;
-    isReplyToBot = false
-    activeDialog: any = null
+    isReplyToBot = false;
+    activeDialog: any = null;
 
     public async handleNewMessage(message: TelegramBot.Message) {
         this.messageMeta = message && message.entities ? message.entities[0] : null;
         this.hasNeededMeta =
             !!this.messageMeta &&
-            (this.messageMeta.type === "bot_command" ||
-                (this.messageMeta.type === "mention" &&
+            (this.messageMeta.type === 'bot_command' ||
+                (this.messageMeta.type === 'mention' &&
                     !!message?.text?.toLowerCase()?.trim()?.includes(process.env.TELEGRAM_BOT_NAME!)));
         this.isReplyToBot =
             message.reply_to_message?.from?.username?.toLowerCase()?.trim() === process.env.TELEGRAM_BOT_NAME?.toLowerCase();
@@ -44,7 +44,7 @@ export class TelegramController {
             }
 
             if (!this.activeDialog) {
-                const regex = new RegExp(`@${process.env.TELEGRAM_BOT_NAME!}|/`, "g");
+                const regex = new RegExp(`@${process.env.TELEGRAM_BOT_NAME!}|/`, 'g');
                 const botCommand = messageText.replace(regex, '').trim();
 
                 this.setActiveDialog(botCommand);
@@ -58,20 +58,20 @@ export class TelegramController {
 
     public async handleCallbackQuery(payload: TelegramBot.CallbackQuery) {
         if (this.activeDialog) {
-            await this.activeDialog.handleCallbackQuery(payload)
+            await this.activeDialog.handleCallbackQuery(payload);
         }
-    }
-
-    private setActiveDialog(dialogName: string) {
-        const key = Object.keys(dialogs).filter(key => key.toLowerCase().includes(dialogName))[0]
-        // @ts-ignore
-        this.activeDialog = new dialogs[key]()
-        this.activeDialog.on('dialog is over', () => {
-            this.activeDialog = null
-        })
     }
 
     public async sendMessage(message: TelegramBot.SendMessageParams) {
         await sendMessage(message);
+    }
+
+    private setActiveDialog(dialogName: string) {
+        const key = Object.keys(dialogs).filter(key => key.toLowerCase().includes(dialogName))[0];
+        // @ts-ignore
+        this.activeDialog = new dialogs[key]();
+        this.activeDialog.on('dialog is over', () => {
+            this.activeDialog = null;
+        });
     }
 }
