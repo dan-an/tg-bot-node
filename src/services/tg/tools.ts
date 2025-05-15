580c7e49e35ac84d55da9795bb98bffaad8b6553
Danilov Andrey <a.danilov@kryptonite.ru>
2025-05-15 11:39:00 +0300

    ni: add shopping list edit

--- src/services/tg/tools.ts
+++ src/services/tg/tools.ts
@@ -10,3 +10,17 @@ export const sendMessage = async (message: TelegramBot.SendMessageParams): Promi
 export const getRandomPhrase = (phraseList: string[]): string => {
     return phraseList[Math.floor(Math.random() * phraseList.length)];
 };
+
+export const editMessage = async (messageId: number, params: TelegramBot.SendMessageParams) => {
+    try {
+        await axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_API_TOKEN}/editMessageText`, {
+            chat_id: params.chat_id,
+            message_id: messageId,
+            text: params.text,
+            parse_mode: params.parse_mode,
+            reply_markup: params.reply_markup,
+        });
+    } catch (error) {
+        console.error('Ошибка редактирования сообщения:', error);
+    }
+};
