7a22586de6628dbd24498fb4dfcc2fa55aa3091b
Danilov Andrey <a.danilov@kryptonite.ru>
2025-05-27 16:40:51 +0300

    ni: add tests for getRandomPhrase

--- tests/services/tg/tools.test.ts
+++ tests/services/tg/tools.test.ts
@@ -4,9 +4,23 @@ import { getRandomPhrase } from '@/tools';
 describe('getRandomPhrase', () => {
     const phraseList = ['привет', 'здравствуй', 'добрый день'];
 
-    it('возвращает строку из списка', () => {
+    it('returns random value from array', () => {
         const values = new Set(phraseList);
         const result = getRandomPhrase(phraseList);
         expect(values.has(result)).toBe(true);
+    });
+
+    it('value is a string', () => {
+        const result = getRandomPhrase(phraseList);
+        expect(typeof result === 'string');
+    });
+
+    it('result is randomized', () => {
+        let result = new Set();
+        for (let i = 0; i < 10; i++) {
+            result.add(getRandomPhrase(phraseList));
+        }
+
+        expect(result.size).toBeGreaterThan(1);
     });
 });
