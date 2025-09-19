ad4013029e876fe1b774ca7f91c8d89e07498ef9
dan-an <danilov-an-s@yandex.ru>
2025-09-19 10:28:59 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -42,7 +42,7 @@ const startScheduler = async () => {
     while (true) {
         const now = dayjs().tz('Europe/Moscow');
 
-        if (now.hour() === 10 && now.minute() === 10) {
+        if (now.hour() === 10 && now.minute() === 35) {
             try {
                 await executeTask();
             } catch (error) {
